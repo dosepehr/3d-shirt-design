22ec9b5af9c5db91dde7c818cb0487a58b854a9f
dosepehr <dosepehr@gmail.com>
2023-08-19 16:32:03 +0330

    add global state

---
+++ src/store/store.js
@@ -0,0 +1,10 @@
+import { proxy } from 'valtio';
+
+const state = proxy({
+    intro: true,
+    colors: ['#ccc', '#EFBD4E', '#80C670', '#726DE8', '#EF674E', '#353934'],
+    decals: ['react', 'threejs', 'logo-tshirt'],
+    selectedColor: '#EFBD4E',
+});
+
+export { state };
