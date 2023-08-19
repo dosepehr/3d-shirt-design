cc7071c16387186c96948d5abe53f73a0882e0c0
dosepehr <dosepehr@gmail.com>
2023-08-19 13:50:36 +0330

    add Canvas and initial Shirt components

---
+++ src/components/Shirt.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Shirt = () => {
+    return (
+        <>
+            <mesh>
+                <boxGeometry />
+                <meshNormalMaterial />
+            </mesh>
+        </>
+    );
+};
+
+export default Shirt;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,21 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
+import { Canvas } from '@react-three/fiber';
+import Shirt from './components/Shirt';
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
+        <Canvas
+            eventSource={document.getElementById('root')}
+            eventPrefix='client'
+            camera={{
+                position: [-1, 0, 2.5],
+                fov: 25,
+            }}
+        >
+            <Shirt />
+        </Canvas>
         <App />
     </StrictMode>
 );
