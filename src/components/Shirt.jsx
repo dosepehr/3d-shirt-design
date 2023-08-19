d0ac315a23582ad2d91fac5f15506e04724df245
dosepehr <dosepehr@gmail.com>
2023-08-19 14:01:35 +0330

    load 3d tshirt model

--- src/components/Shirt.jsx
+++ src/components/Shirt.jsx
@@ -1,14 +1,24 @@
-import React from 'react';
+import { Environment, useGLTF } from '@react-three/drei';
 
-const Shirt = () => {
+const Shirt = (props) => {
+    const { nodes, materials } = useGLTF('/model/shirt_baked.glb');
     return (
         <>
-            <mesh>
-                <boxGeometry />
-                <meshNormalMaterial />
-            </mesh>
+            <ambientLight intensity={0.5} />
+            <Environment preset='city' />
+
+            <group {...props} dispose={null}>
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.T_Shirt_male.geometry}
+                    material={materials.lambert1}
+                />
+            </group>
         </>
     );
 };
 
 export default Shirt;
+
+useGLTF.preload('/shirt_baked.glb');
